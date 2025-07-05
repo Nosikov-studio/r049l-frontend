3ce618a6ce94452dd5d23394baf1b29c94f4fe59
Nos <zvezdazanatih@gmail.com>
2025-07-05 13:40:07 +0700

    5

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  // Всё в порядке! Можно работать
-  console.log('Всё в порядке! Можно работать!!!')
-  navigator.mediaDevices.getUserMedia({video:true, audio: true})
-} else {
-  console.warn('Ваш браузер не поддерживает медиаустройства');
+///////////////// 1) доступ камере////////////////////////
 
+// if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//   // Всё в порядке! Можно работать
+//   console.log('Всё в порядке! Можно работать!!!')
+//   navigator.mediaDevices.getUserMedia({video:true, audio: true})
+// } else {
+//   console.warn('Ваш браузер не поддерживает медиаустройства');
+
+// }
+
+////////////2) Список доступных устройств ///////////////////////
+
+async function getDevices() {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+    devices.forEach(device => {
+      console.log(device.kind, device.label, device.deviceId);
+    });
+  
 }
+
+getDevices();
