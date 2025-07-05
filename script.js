7c1458bc596b010c1e35d3ae2691f96a87e37b76
Nos <zvezdazanatih@gmail.com>
2025-07-05 14:03:57 +0700

    9

--- script.js
+++ script.js
@@ -37,6 +37,23 @@
 
 /////////////////4) Новое изменения в устройствах
 
-navigator.mediaDevices.addEventListener('devicechange', ()=> {
-  console.log("Device changed");
-})
+// navigator.mediaDevices.addEventListener('devicechange', ()=> {
+//   console.log("Device changed");
+// })
+
+/////////////5) Работа с трэками //////////////////////
+
+navigator.mediaDevices.getUserMedia({video:true})
+.then(stream => {
+  const [videoTrack] = stream.getVideoTracks();
+  return videoTrack.applyConstraints({
+    width: 1280,
+    height: 720
+  }).then(()=> { 
+    console.log('Разрешение применено');
+  })
+  .catch(error => {
+    console.error('ошибка доступа к устройству', error);
+  });
+});
+
