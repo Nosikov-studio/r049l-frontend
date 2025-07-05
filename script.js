9794cac6dd8d81a760680ca3bb16aba8b23c1d2a
Nos <zvezdazanatih@gmail.com>
2025-07-05 11:17:46 +0700

    2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   // Всё в порядке! Можно работать
-  console.log('Всё в порядке! Можно работать')
+  console.log('Всё в порядке! Можно работать!!!')
 } else {
   console.warn('Ваш браузер не поддерживает медиаустройства');
 
