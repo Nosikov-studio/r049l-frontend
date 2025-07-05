2f13a048224b46d6ec3c88f58137f459b6c38cdd
Nos <zvezdazanatih@gmail.com>
2025-07-05 13:26:34 +0700

    4

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   // Всё в порядке! Можно работать
   console.log('Всё в порядке! Можно работать!!!')
+  navigator.mediaDevices.getUserMedia({video:true, audio: true})
 } else {
   console.warn('Ваш браузер не поддерживает медиаустройства');
 
